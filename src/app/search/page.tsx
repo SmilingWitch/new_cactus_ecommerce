fb7648b32314f9fa7cd44876a07965722e423299
SmilingWitch <danielitayero@gmail.com>
2024-02-25 21:24:36 -0500

    feat: add search/

---
+++ src/app/search/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+
+import Search from "../components/search/Search";
+
+export default function Home() {
+  return (
+        <Search/>
+  );
+}
