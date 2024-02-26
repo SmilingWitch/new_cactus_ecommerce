dbaaa668b776044c623ae5174ec51f6dc3c775cf
SmilingWitch <danielitayero@gmail.com>
2024-02-25 20:48:00 -0500

    feat: add description offer, blog and trending to Home

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,10 +1,18 @@
 import style from "../../../public/styles/Home.module.css"
 import Image from "next/image"
 import { GoSearch } from "react-icons/go";
+import CategoriesCard from "./CategoriesCard";
+import { PiCactusFill } from "react-icons/pi";
+import OfferCard from "./OffertCard";
+import TrendigCard from "./TrendingCard";
+import PickCard from "./PickCard";
+import BlogCard from "./BlogCrad";
 
 export default function Home(){
     return(
         <div className={style.cont}>
+            <div className={style.bx}>
+
             <div className={style.front_page}>
                 <div className={style.header}>
                     <div>
@@ -29,7 +37,106 @@ export default function Home(){
                     <Image 
                         src="/images/junior-rodriguez-gsCecg8nrAY-unsplash.jpg"  
                         layout="fill"
-                        objectFit="cover"></Image>
+                        objectFit="cover"
+                        alt = "Description"></Image>
+                </div>
+            </div>
+
+            <div className={style.categories}>
+                <h2>Categories</h2>
+                <div className={style.categories_bx}>
+                    <CategoriesCard name="Cactus" url = "hbh" redirect_url=""/>
+                    <CategoriesCard name="Succulents" url = "hbh" redirect_url =""/>
+                    <CategoriesCard name="Fertilizers" url = "hbh" redirect_url =""/>
+                    <CategoriesCard name="Planters" url = "hbh" redirect_url =""/>
+                    <CategoriesCard name="Tools &" url = "hbh" redirect_url =""/>
+                    <CategoriesCard name="Books &" url = "hbh" redirect_url =""/>
+                </div>
+            </div>
+
+            <div className={style.offets_bx}>
+                <div className={style.special}>
+                    <div className={style.special_header}>
+                        <div className={style.special_icon_bx}>
+                            <PiCactusFill className={style.special_icon}/>
+                        </div>
+                        <div className={style.header1}>
+                            Special offers
+                        </div>
+
+                    </div>
+                    <div className={style.offer}>
+                        <OfferCard  url = "/images/conjunto_minimalista_de_macetas_para_plantas_si(1).jpg"/>
+                        <OfferCard  url = "/images/conjunto_minimalista_de_macetas_para_plantas_si(1).jpg"/>
+                    </div>
+                    <div className={style.counter}>
+                        <div className={style.counter_header}>
+                            <div className={style.principal}>Limited stock</div>
+                            <div className={style.secundary}>Hurry up!</div>
+                            <div className={style.hour}>
+                                <span>1</span>
+                                <span>0</span>
+                                <span>:</span>
+                                <span>5</span>
+                                <span>0</span>
+                            </div>
+                        </div>
+                        
+
+                    </div>
+                </div>
+                <div className={style.trending}>
+                    <div className={style.trending_cont}>
+                        <div className={style.special_header}>
+                            <div className={style.special_icon_bx}>
+                                <PiCactusFill className={style.special_icon}/>
+                            </div>
+                            <div className={style.header1}>
+                                Trending products
+                            </div>
+                        </div>
+                        <div className={style.trendingBtx}>
+                            <TrendigCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si(3).jpg" price= {5.00} old_price = {8.00}/>
+                            <TrendigCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si(3).jpg" price= {5.00} old_price = {8.00}/>
+                            <TrendigCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si(3).jpg" price= {5.00} old_price = {8.00}/>
+                            <TrendigCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si(3).jpg" price= {5.00} old_price = {8.00}/>
+                            <TrendigCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si(3).jpg" price= {5.00} old_price = {8.00}/>
+                            <TrendigCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si(3).jpg" price= {5.00} old_price = {8.00}/>
+                        </div>
+
+                    </div>
+                    <div className={style.picks}>
+
+                        <div className={style.special_header}>
+                            <div className={style.special_icon_bx}>
+                                <PiCactusFill className={style.special_icon}/>
+                            </div>
+                            <div className={style.header1}>
+                                Popular Picks
+                            </div>
+                        </div>
+                        <div className={style.pick}>
+                            <PickCard   url = "/images/conjunto_minimalista_de_macetas_para_plantas_si(6).jpg" header = "Cactus accesories" secundary_header="Pots, Tools, Decorations"/>
+                            <PickCard   url = "/images/conjunto_minimalista_de_macetas_para_plantas_si(6).jpg" header = "Succulent soil mix" secundary_header="Perfect for your plant collection"/>
+                        </div>
+
+                    </div>
+                    
+                </div>
+
+            </div>
+
+            </div>
+         
+
+            <div className={style.blog}>
+                <div className={style.bx}>
+                    <h2>Visit our blog</h2>
+                    <div className={style.blog_card_bx}>
+                        <BlogCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si(7).jpg" header="5 Tips for Healthy Succulents" secundary_header="Learn how to care for your succulents and keep them thriving"/>
+                        <BlogCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si(2).jpg" header="5 Tips for Healthy Succulents" secundary_header="Learn how to care for your succulents and keep them thriving"/>
+                        <BlogCard url="/images/conjunto_minimalista_de_macetas_para_plantas_si.jpg" header="5 Tips for Healthy Succulents" secundary_header="Learn how to care for your succulents and keep them thriving"/>
+                    </div>
 
                 </div>
             </div>
