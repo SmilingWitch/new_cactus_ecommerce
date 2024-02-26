c37be3b661842c3adb765a6fa046aca01251a092
SmilingWitch <danielitayero@gmail.com>
2024-02-25 21:24:20 -0500

    feat: add search/

---
+++ src/app/components/search/Search.tsx
@@ -0,0 +1,10 @@
+import style from "../../../../public/styles/Search.module.css"
+
+
+export default function Search(){
+    return(
+        <div className={style.cont}>
+            <h1>Search</h1>
+        </div>
+    )
+}
