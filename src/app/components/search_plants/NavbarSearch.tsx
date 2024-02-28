f5a1af9e36f36e4793ea003428333d572cdc7f2c
SmilingWitch <danielitayero@gmail.com>
2024-02-27 22:04:10 -0500

    feat: add visible prop to NavbarSearch

--- src/app/components/search_plants/NavbarSearch.tsx
+++ src/app/components/search_plants/NavbarSearch.tsx
@@ -1,14 +1,23 @@
+"use client"
 import style from "../../../../public/styles/NavbarSearch.module.css"
 import Image from "next/image"
 import Link from "next/link"
 import { FaBasketShopping } from "react-icons/fa6";
 import { FaUser } from "react-icons/fa";
 import { GoSearch } from "react-icons/go";
+import { TiThMenu } from "react-icons/ti";
+import { useState } from "react";
 
-export default function NavbarSearch(){
+interface MyComponentProps {
+    setVisible: (visible: boolean) => void;
+  }
+
+
+export default function NavbarSearch(props: React.PropsWithChildren<MyComponentProps>){
+
+
     return(
         <div className={style.cont}>
-
                 <div className={style.logo}>
                     <Link href="/home">
                         <Image src = "/images/[removal.ai]_db996c9f-aad7-4611-8d6b-c706cacb0c28-screenshot-2024-02-25-200736.png"
@@ -26,19 +35,18 @@ export default function NavbarSearch(){
                     </div>
                     
                 </div>
-
-
-            
             <div className={style.icon_bx}>
-                
                 <div className={style.icon}>
                     <FaBasketShopping/>
                 </div>
                 <div className={style.icon}>
                     <FaUser/>
                 </div>
-                
+                <div className={style.icon} onClick={() => props.setVisible(true)}>
+                    <TiThMenu/>
+                </div>
             </div>
+            
         </div>
     )
 }
