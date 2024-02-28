ba32c6270bebb04f759c7954690fa1f6d251f6ca
SmilingWitch <danielitayero@gmail.com>
2024-02-28 14:09:29 -0500

    feat: add cart/

---
+++ src/app/cart/layout.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import SideBar from "@/components/SideBar";
+import NavbarSearch from "@/components/search_plants/NavbarSearch";
+import {useState} from "react"
+
+import  { ReactNode } from "react";
+
+interface RootLayoutProps {
+  children: ReactNode; // Define el tipo de children
+}
+
+
+export default function RootLayout({ children }: RootLayoutProps) {
+
+const [visible, setVisible] = useState(false)
+
+
+ return (
+   <>
+      <SideBar visible = {visible} setVisible={setVisible} />
+      <NavbarSearch setVisible={setVisible}/>
+     <main >{children}</main>
+ 
+   </>
+ );
+}
+
---
+++ src/app/cart/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import styles from "../page.module.css";
+import Cart from "@/components/Cart";
+
+
+export default function Categories() {
+  return (
+      <Cart/>
+    
+  );
+}
