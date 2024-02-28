95cd6e68448942f9b2446dda51cc0961384353a9
SmilingWitch <danielitayero@gmail.com>
2024-02-28 14:24:19 -0500

    ref: fix url of home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
-import Home1 from "./components/Home"
+import Home1 from "../components/Home"
 
 export default function Home() {
   return (
