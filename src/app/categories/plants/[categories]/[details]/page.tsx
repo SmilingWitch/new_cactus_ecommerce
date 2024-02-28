0fb3e05622f47a162ead4d99d1ae1465617e1f15
SmilingWitch <danielitayero@gmail.com>
2024-02-27 21:59:31 -0500

    feat: add plants/[categories]/[detail]

---
+++ src/app/categories/plants/[categories]/[details]/page.tsx
@@ -0,0 +1,14 @@
+
+import Image from "next/image";
+import styles from "../page.module.css";
+import PlantCategories from "@/app/components/search_plants/PlantCategories";
+import SearchAccesories from "@/app/components/search_accesories/SearchAccesories";
+import Details from "@/app/components/Details";
+
+
+export default function Categories() {
+  return (
+    <Details/>
+    
+  );
+}
