003124458933a8a5c1dc04866779c333b556d64c
SmilingWitch <danielitayero@gmail.com>
2024-02-28 14:14:12 -0500

    ref: change import url in shop/page.tsx

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import styles from "../page.module.css";
-import PlantCategories from "@/app/components/search_plants/PlantCategories";
-import SearchAccesories from "@/app/components/search_accesories/SearchAccesories";
-import Shop from "../components/Shop";
+import Shop from "@/components/Shop";
+
 
 
 export default function Categories() {
