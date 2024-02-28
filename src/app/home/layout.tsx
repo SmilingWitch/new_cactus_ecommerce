967842ce3ede56b0e1aee1b0ea7aad305990176b
SmilingWitch <danielitayero@gmail.com>
2024-02-27 22:37:50 -0500

    fix: add type of children in layouts

--- src/app/home/layout.tsx
+++ src/app/home/layout.tsx
@@ -1,13 +1,14 @@
 "use client"
-
-
-
 import {useState} from "react"
 import Navbar from "../components/Navbar"
+import  { ReactNode } from "react";
+
+interface RootLayoutProps {
+  children: ReactNode; // Define el tipo de children
+}
 
 
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
 
 
  return (
--- src/app/shop/layout.tsx
+++ src/app/shop/layout.tsx
@@ -2,10 +2,13 @@
 import {useState} from "react"
 import NavbarSearch from "../components/search_plants/NavbarSearch";
 import SideBar from "../components/SideBar";
+import  { ReactNode } from "react";
 
+interface RootLayoutProps {
+  children: ReactNode; // Define el tipo de children
+}
 
-
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
 
 
   const [visible, setVisible] = useState(false)
