5cc8fddccdf9342aa355280381463a588d651bd7
SmilingWitch <danielitayero@gmail.com>
2024-03-06 13:07:07 -0500

    feat: obtein details from localStorage in Details

--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -7,17 +7,52 @@ import Link from "next/link"
 import { RiArrowLeftSLine } from "react-icons/ri";
 import { useRouter } from 'next/navigation';
 import ProductCardSearch from "./search_plants/ProductCardSearch";
+import { useState, useEffect } from "react";
+import { useParams } from 'next/navigation'
 
 
 export default function Details(){
 
-
+    const param = useParams()
+    const id = Number(param.details); // Elimina "/plants/" de la URL
+    console.log("ID",id)
     const router = useRouter();
+
+    // variable
+    let allFromsessionStorage = [];
+    let filteredCategory = []
+    if (typeof window !== 'undefined') {
+    const item = sessionStorage.getItem('all_categories');
+    if (item !== null) {
+        allFromsessionStorage = JSON.parse(item) || [];
+        filteredCategory = allFromsessionStorage.filter((detail: any) => detail.id === id);
+    }
+}
+
+    // states
+    const [res, SetRes] = useState(allFromsessionStorage)
+    const [isMounted, setIsMounted] = useState(false);
+    const [details, SetDetails] = useState(filteredCategory[0]) 
+
+    console.log("Image", details.image)
+    
 
     const handleBack = () => {
         console.log("back")
       router.back();
     };
+
+    useEffect(() => {
+        setIsMounted(true); 
+      }, []);
+
+
+
+
+
+      if (!isMounted) {
+        return null; // Or some placeholder content
+       }
 
     return(
         <div className={style.cont}>
@@ -32,7 +67,7 @@ export default function Details(){
                     <div className={style.gallery}>
                         <div className={style.image1}>
                                 <Image 
-                                src="/images/kari-shea-tcgMBsW4zlU-unsplash.jpg"  
+                                src={`https://cactusshopi.onrender.com${details.image}`}
                                 layout="fill"
                                 objectFit="cover"
                                 alt = "Description"></Image>
@@ -40,7 +75,7 @@ export default function Details(){
                             <div className={style.product_info}>
                                 <div className={style.info}>
                                     <div className={style.name}>
-                                        <span>Name</span>
+                                        <span>{details.name}</span>
                                         <div className={style.stars_bx}>
                                         <GoStarFill/>
                                         <GoStarFill/>
@@ -49,7 +84,7 @@ export default function Details(){
                                         <GoStar/>
                                         </div>
                                     </div>
-                                    <div className={style.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sit cumque corrupti commodi hic dolorum! Nostrum, tempora culpa! Consectetur, facilis modi nihil iste vel in. Labore quis illum culpa officiis.</div>
+                                    <div className={style.text}>{details.description}</div>
                                     <div className={style.btn}>
                                         <button>Add to cart</button>
                                     </div>
@@ -65,13 +100,13 @@ export default function Details(){
                     </div>
                     <div className={style.images_bx}>
                         <ProductCardSearch url ="/images/britta-preusse-p7KKwjP3s68-unsplash.jpg" 
-                                            price = {15} amount={1} url_redirect = "/dd"/>
+                                            price = {15} amount={1} url_redirect = "/dd" name = "Name"/>
                         <ProductCardSearch url ="/images/andrea-rico-Npfkyf94cik-unsplash.jpg" 
-                                            price = {15} amount={1} url_redirect = "/dd"/>
+                                            price = {15} amount={1} url_redirect = "/dd" name = "Name"/>
                         <ProductCardSearch url ="/images/stephanie-harvey-T0inbt7nRME-unsplash.jpg" 
-                                            price = {15} amount={1} url_redirect = "/dd"/>
+                                            price = {15} amount={1} url_redirect = "/dd" name = "Name"/>
                         <ProductCardSearch url ="/images/britta-preusse-p7KKwjP3s68-unsplash.jpg" 
-                                            price = {15} amount={1} url_redirect = "/dd"/>
+                                            price = {15} amount={1} url_redirect = "/dd" name = "Name"/>
                     </div>
                     
                 </div>
