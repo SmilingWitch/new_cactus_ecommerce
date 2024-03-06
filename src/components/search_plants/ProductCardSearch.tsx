51a9c5ef70c9883a5958f465a2a8f2bc083f4cd8
SmilingWitch <danielitayero@gmail.com>
2024-03-06 13:11:07 -0500

    feat: obtein the name from prop in ProductCardSearch

--- src/components/search_plants/ProductCardSearch.tsx
+++ src/components/search_plants/ProductCardSearch.tsx
@@ -1,3 +1,4 @@
+"use client"
 import style from "../../../public/styles/ProductCardSearch.module.css"
 import Image from "next/image"
 import { IoHeartOutline } from "react-icons/io5";
@@ -8,23 +9,25 @@ interface MyComponentProps {
     url: string;
     price: number;
     amount: number;
-    url_redirect: string
+    url_redirect: string;
+    name: string
   }
-
+  
 export default function ProductCardSearch(props: React.PropsWithChildren<MyComponentProps>){
+    
     return(
         <Link href={props.url_redirect}>
             <div className={style.card}>
                 <div className={style.image_bx}>
                     <Image 
-                    src={props.url} 
+                    src={`https://cactusshopi.onrender.com${props.url}`}/*"/images/diego-lozano-kGnRkbKnqq4-unsplash.jpg"*/
                     layout="fill"
                     objectFit="cover"
                     alt = "Description"></Image>
                     <div className={style.heart}><IoHeartOutline className={style.icon}/></div>
                 </div>
                 <div className={style.info}>
-                    <div className={style.name}>Name</div>
+                    <div className={style.name}>{props.name}</div>
                     <div className={style.info_price}>
                         <div>$ {props.price.toFixed(2)} </div> 
                         <div>/ {props.amount} pc</div> 
