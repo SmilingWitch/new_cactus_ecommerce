4939e46775760ecf62ee5e4a43728fe5e55f3e31
SmilingWitch <danielitayero@gmail.com>
2024-03-12 17:21:38 -0400

    feat: add increment(), decrement() and deleteItem() to CartCard

--- src/components/CartCard.tsx
+++ src/components/CartCard.tsx
@@ -1,53 +1,114 @@
+"use client"
 import style from "../../public/styles/CartCard.module.css"
 import Image from "next/image"
 import { FaPlus } from "react-icons/fa";
 import { FaMinus } from "react-icons/fa";
 import { FaTrashCan } from "react-icons/fa6";
+import { useState, useEffect } from "react";
 
-export default function CartCard(){
+interface MyProps {
+    id: number,
+    qty: number,
+    name: string,
+    image: string,
+    cost: number,
+    updateProduct: (products: boolean) => void;
+}
+
+export default function CartCard(props: React.PropsWithChildren<MyProps>){
+
+    let products: any = []
+    if (typeof window !== 'undefined') {
+        const item = localStorage.getItem('products');
+        if (item !== null) {
+            products = JSON.parse(item) || [];
+        }
+    }
+
+/*-----------------------------INCREMENT ITEM---------------------------- */
+    const increment = (qty: number, products: any) => {
+        // Encontrar el índice del producto por ID
+        const productIndex = products.findIndex((product: any) => product.id === props.id);
+        if (productIndex > -1) {
+            // Verificar si la cantidad es menor o igual a 20
+            if (qty < 20) {
+                products[productIndex].qty += 1;
+            } else {
+                products[productIndex].qty = 20;
+            }
+            // Actualizar el sessionStorage
+            localStorage.setItem('products', JSON.stringify(products));
+    
+            // Actualizar el estado de los productos 
+            props.updateProduct(products[productIndex]);
+        }
+    };
+
+/*-----------------------------DECREMENT ITEM---------------------------- */
+    const decrement = (qty: number, products: any) => {
+        // Encontrar el índice del producto por ID
+        const productIndex = products.findIndex((product: any) => product.id === props.id);
+            console.log("productIndex",productIndex)
+            // Verificar si la cantidad es menor o igual a 20
+            if (qty > 1) {
+                products[productIndex].qty -= 1;
+                // Actualizar el sessionStorage
+                localStorage.setItem('products', JSON.stringify(products));
+    
+                // Actualizar el estado de los productos 
+                props.updateProduct(products[productIndex]);  
+            } else {
+                const filtered = products.filter((product: any) => product.id !== props.id);
+                console.log("FILTERED",filtered)
+                // Almacenar el arreglo filtrado en el almacenamiento local
+                localStorage.setItem('products', JSON.stringify(filtered));
+                window.dispatchEvent(new Event('storage'));
+                window.dispatchEvent(new CustomEvent('cart'));
+            } 
+    };
+
+    /*-----------------------------DELETE ITEM---------------------------- */
+    const deleteItem = (id: number, products: any) => {
+        const filtered = products.filter((product: any) => product.id !== props.id);
+        console.log("FILTERED",filtered)
+        // Almacenar el arreglo filtrado en el almacenamiento local
+        localStorage.setItem('products', JSON.stringify(filtered));
+        window.dispatchEvent(new Event('storage'));
+        window.dispatchEvent(new CustomEvent('cart'));
+        
+    };
+
+    
+    
     return(
         <div className={style.card}>
             <div className={style.info_bx}>
                 <div className={style.image_bx}>
-                    <Image src = "/images/conjunto_minimalista_de_macetas_para_plantas_si(4).jpg"
+                    <Image src = {`https://cactusshopi.onrender.com${props.image}`}
                             layout="fill"
                             objectFit="cover"
                             alt = "Description"/>
                 </div>
                 <div className={style.info}>
-                    <div className={style.name}>Name</div>
-                    <div className={style.price}>$ 5.00</div>
+                    <div className={style.name}>{props.name}</div>
+                    <div className={style.price}>$ {props.cost}</div>
                 </div>
             </div>
             
             <div className={style.bx}>
                 <div className={style.qty_info}>
-                    <div className={style.btn}>
-                        <FaTrashCan className={style.icon}/>
+                    <div className={style.btn} onClick={() => deleteItem(props.id, products)}>
+                        <FaTrashCan className={style.icon} />
                     </div>
                     <div className={style.qty}>
-                        {/*<div className={style.qty_bx}>
-                            <span>Quantity: </span>
-                            <span>1</span>
-                        </div>*/}
                         <div className={style.btn_bx}>
-                            <span className={style.icon_qty}><FaMinus/></span>
-                            <span>1</span>
-                            <span className={style.icon_qty}><FaPlus/></span>
+                            <span className={style.icon_qty} onClick={() => decrement(props.qty, products)}><FaMinus /></span>
+                            <span>{props.qty}</span>
+                            <span className={style.icon_qty} onClick={() => increment(props.qty, products)}><FaPlus /></span>
                         </div>
                     </div>
-                    {/*<div className={style.totalP}>
-                         <span>Total: </span>
-                         <span>$ 5.00</span>   
-                </div>*/}
-
-                    
                 </div>
-                
             </div>
-            
-            
-
         </div>
     )
 }
