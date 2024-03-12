3e868a716728dd1bea3affe31be8c00437bc8ec0
SmilingWitch <danielitayero@gmail.com>
2024-03-12 17:23:40 -0400

    feat: add component when the page is not mounted in Shop

--- src/components/Shop.tsx
+++ src/components/Shop.tsx
@@ -5,6 +5,7 @@ import HeaderCategories from "./HeaderCategories"
 import ProductCardSearch from "./search_plants/ProductCardSearch"
 import { IoIosArrowDown } from "react-icons/io";
 import axios from "axios"
+import Skeletom from "./Skeletom"
 
 export default function Shop(){
 
@@ -46,13 +47,9 @@ export default function Shop(){
 
 
     const all = async () =>{
-
-
         try {
             const response = await axios.get('https://cactusshopi.onrender.com/plants/plant/');
-            /*SetRes(response.data)*/
             console.log(response.data)
-
             const item = sessionStorage.getItem('all_categories');
             SetRes(item ? JSON.parse(item) : null);
 
@@ -62,7 +59,6 @@ export default function Shop(){
             if (item2 !== null) {
                 codigossessionStorage = JSON.parse(item2);
             }
-
 
             // Obtén los datos de la respuesta de la petición
             let codigosResponse = response.data;
@@ -100,7 +96,53 @@ export default function Shop(){
 
 
      if (!isMounted) {
-        return null; // Or some placeholder content
+        return (
+
+            <div className={style.cont}>
+      <div className={style.header}>
+          <HeaderCategories url = "/images/icons8-shop-80.png" header = "Shop"/>
+      </div>
+      <div className="bx">
+      <div className={style.search}>
+              <div className={style.filter}>
+                  <div className={style.sort}>
+                      Sort by:
+                  </div>
+                  <div className={style.input_bx}>
+                      <div className={style.input} onClick = {()=> setVisible(!visible)}>
+                          <div className={style.selectedType}>{selectedOption}</div>
+                          <div className={style.selectIcon} onClick = {()=> setVisible(!visible)}><IoIosArrowDown/></div>
+                      </div>
+                  
+                  
+                      {visible && <div className={style.options}>
+                          {options.map((option, index) => (
+                            <div key={index} onClick={() =>{ handleClick(option); setVisible(false)}} className={style.option}>
+                              {option}
+                            </div>
+                          ))}
+
+                    </div>}
+                  </div>
+                  
+              </div>
+          </div>
+          </div>
+          <div className="bx">
+
+            <div className={style.plats_bx}>
+                <Skeletom/>
+                <Skeletom/>
+                <Skeletom/>
+                <Skeletom/>
+                <Skeletom/>
+                <Skeletom/>
+            </div>
+
+          </div>
+      </div>
+
+        );
        }
 
 
@@ -137,14 +179,24 @@ export default function Shop(){
           </div>
           </div>
           <div className="bx">
+          { !isMounted || res === null ?
+            <div className={style.plats_bx}>
+                <Skeletom/>
+                <Skeletom/>
+                <Skeletom/>
+                <Skeletom/>
+                <Skeletom/>
+                <Skeletom/>
+            </div>
+            :
             <div className={style.plats_bx}>
             {res.map((item: any) => {
-                    return <ProductCardSearch key = {item.id} url = {item.image} price= {item.cost} amount = {1} url_redirect= {`/shop/${item.id}`} name = {item.name}/>
+                    return <ProductCardSearch key = {item.id} url = {item.image} price= {item.cost} amount = {1} url_redirect= {`/shop/${item.id}`} name = {item.name} />
             })}
                
-         
+                
             </div>
-
+        }
           </div>
       </div>
     )
